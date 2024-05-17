4ebdf2f22884dcee25da54323ee5709b6a46473b
CFBJPQ3-QSH\Admin <lizhenhua.0769@bytedance.com>
2024-05-17 16:50:41 +0800

    Revert "将mesh tool代码合过来"
    This reverts commit bc8ce4f4177ce1026ef97d3c156de4dc89aab85e.

--- Source/ToolsFrameworkDemo/ToolsFrameworkDemo.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ToolsFrameworkDemo.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ToolsFrameworkDemo, "ToolsFrameworkDemo" );
